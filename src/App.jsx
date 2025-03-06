a8a3d3ad45df08500da856a84d2bc4d9b9ee6fb1
MatamaPeter <peter24matama@gmail.com>
2025-03-06 20:08:48 +0300

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
 import "./App.css"
+import { useEffect } from "react"
 function App() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  const [inputValue, setInputValue] = useState()
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "%", "."]
+
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        console.log(eval(inputValue));
+      } catch (e) {
+        console.error(e);
+      }
+    },1000)
+  }
+  ,[inputValue])
+  
+ 
+  
+
   const operators = ["+","/","-","*"]
 
   const numberElements = numbers.map((number) => {
@@ -10,21 +29,32 @@ function App() {
     return <button key={operator}>{operator}</button>
   })
 
+  
   return (
     <div>
       <div className="container">
         <div className="screen">
-          <div className="display"></div>
-          <div className="input"></div>
+          <div className="input">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e)=>setInputValue(e.target.value)}
+            />
+          </div>
+          <div className="display">
+            {inputValue}
+          </div>
+
+        </div>
           <div className="buttons">
             <div className="numberbtn">
               {numberElements}
             </div>
             <div className="operatorbtns">
               {operatorElements}
-            </div>
           </div>
-        </div>
+
+          </div>
       </div>
     </div>
   )
